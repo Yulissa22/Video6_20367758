bd351fe3db9f793e9a8c5bc5d934f92c6e173ca6
MINEDUCYT <MINEDUCYT@DESKTOP-OAO2BUA>
2023-06-27 21:46:16 -0600

    Agregar archivos de proyecto.

---
+++ Video6_20367758/Video6_20367758/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Video6_20367758
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Entry_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            string pasado = e.OldTextValue;
+            string nuevo = e.NewTextValue;
+
+        }
+
+        private void Entry_Completed(object sender, EventArgs e)
+        {
+            string text = ((Entry)sender).Text;
+        }
+    }
+}
+ 
